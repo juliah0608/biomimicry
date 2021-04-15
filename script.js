9c83a75fff1b36f9412d758ff048d3f1b5bb281a
juliah0608 <hercj191@newschool.edu>
2021-04-15 14:42:00 -0400

    yeah

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $("button").click(function(){
+    $("div").animate({
+      left: '250px',
+      opacity: '0.5',
+      height: '150px',
+      width: '150px'
+
+    });
+  });
+});
+
+
